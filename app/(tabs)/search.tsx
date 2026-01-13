bf035190481b4d51b4ff06a1c06de871ae7104b0
udara88 <udarawickramasinghe88@gmail.com>
2026-01-13 16:10:13 +0530

    Implemented fetch menu and categoies functionality

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,24 @@
 
 import {Text, View} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
+import useAppwrite from "@/lib/useAppwrite";
+import {getCategories, getMenu} from "@/lib/appwrite";
+import {useLocalSearchParams} from "expo-router";
+import {useEffect} from "react";
 
 function Search() {
+    const {category,query} = useLocalSearchParams<{query:string,category:string}>()
+    const {data,refetch,loading} = useAppwrite({fn:getMenu, params:{category, query, limit:6}});
+    const {data:categories} = useAppwrite({fn:getCategories,});
+
+
+    useEffect(() => {
+        refetch({category,query,limit:6});
+    }, [category,query]);
+
+    console.log(data);
     return (
-        <SafeAreaView>
+        <SafeAreaView >
             <Text>Search</Text>
         </SafeAreaView>
     );
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,6 +1,6 @@
 
 import {Account, Avatars, Client, Databases, ID, Query,Storage} from "react-native-appwrite"
-import {CreateUserParams, SignInParams, User} from "@/type";
+import {CreateUserParams, GetMenuParams, SignInParams, User} from "@/type";
 
 
 
@@ -70,5 +70,37 @@ export  const getCurrentUser = async () => {
 
     }catch (e) {
         throw new Error(e as string)
+    }
+}
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+    try {
+        const queries: string[] = [];
+
+        if(category) queries.push(Query.equal('categories', category));
+        if(query) queries.push(Query.search('name', query));
+
+        const menus = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.menuCollectionId,
+            queries,
+        )
+
+        return menus.documents;
+    } catch (e) {
+        throw new Error(e as string);
+    }
+}
+
+export const getCategories = async () => {
+    try {
+        const categories = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.categoriesCollectionId,
+        )
+
+        return categories.documents;
+    } catch (e) {
+        throw new Error(e as string);
     }
 }
